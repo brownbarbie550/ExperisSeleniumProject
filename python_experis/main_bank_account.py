c9a2e3b77f6595ec5fa64f83647ea0adb2ecfdde
brownbarbie550 <dagihabesha114@gmail.com>
2025-02-23 14:00:41 +0200

    Delete python_experis directory

--- python_experis/main_bank_account.py
+++
@@ -1,5 +0,0 @@
-from Bank_Account import Bank_Account
-
-my_account = Bank_Account("dagi", 1234)
-my_account.print_details()
-print(my_account) #will print the address in the memory
--- python_experis/methods_global_local_arguments.py
+++
@@ -1,8 +0,0 @@
-def method1():
-    global a #changes the parameter that outside the function and in the main
-    a += 5
-    print(a)
-
-a = 10
-method1()
-print(a)
